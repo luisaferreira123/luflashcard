a25fb1904ed4c9d37f632ad51c774aead7f016e8
luisaferreira123 <carobertazuli@gmail.com>
2024-11-28 11:08:52 -0300

    Add files via upload

---
+++ flashcard/perguntas (3).js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciência',
+    'O que é Fotossíntese?',
+    'É um processo biológico realizado por plantas, algas e algumas bactérias, que transforma a luz solar em energia química.'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem é considerado o melhor jogador de futebol do Mundo?'
+    'Lionel Messi está entre o top 3 jogadores melhores do Mundo!'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Qual é a função de um primer?',
+    'O primer serve para preparar a pele para receber a maquiagem, fechando os poros e deixando a pele aveludada.'
+)
+
+criaCartao(
+    'Comida',
+    'Qual a comida mais consumida pelos brasileiros?',
+    'Sem dúvidas, o tradicional ARROZ e FEIJÃo!'
+)
